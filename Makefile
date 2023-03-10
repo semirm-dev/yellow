c06a647f53191cb5e144c03c5c111ca2667aa22f
semirmahovkic <semirmahovkic@192.168.1.8>
2023-03-10 17:10:33 +0100

    yellow'

---
+++ Makefile
@@ -0,0 +1,2 @@
+redpanda::
+	docker run -p 8080:8080 -e KAFKA_BROKERS=host.docker.internal:9092 docker.redpanda.com/redpandadata/console:latest
--- src/main/java/ns/yellow/offer/OfferService.java
+++ src/main/java/ns/yellow/offer/OfferService.java
@@ -2,8 +2,11 @@ package ns.yellow.offer;
 
 import ns.yellow.offer.dto.EventDto;
 import ns.yellow.offer.dto.MarketDto;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
@@ -44,4 +47,14 @@ public class OfferService {
 
     // TODO: listen for markets and events kafka topics
     // TODO: update local state of markets and events with newly received markets and events from kafka
+
+    @KafkaListener(topics = "market", groupId = "offer")
+    public void listenMarkets(String message) {
+        System.out.println("kafka markets received: " + message);
+    }
+
+    @KafkaListener(topicPartitions = @TopicPartition(topic = "event", partitions = {"0", "1"}), groupId = "offer")
+    public void listenEvents(ConsumerRecord<String, String> record) {
+        System.out.println("kafka events received on partition " + record.partition() + ", message: " + record.key());
+    }
 }
